9f9ef91132efe06eaf28204a5eacb11f8dda2808
Atsuhy <zennu2023@gmail.com>
2026-03-16 09:12:51 +0100

    második
    asd

--- aTudasFajaSolution2/ClassLibraryATudasFaja/obj/Debug/net10.0/ClassLibraryATudas.AssemblyInfo.cs
+++ aTudasFajaSolution2/ClassLibraryATudasFaja/obj/Debug/net10.0/ClassLibraryATudas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibraryATudas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4072ff01d157fb13abbe7b399b24d30e271de06")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibraryATudas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibraryATudas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
